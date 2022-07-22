127555b228b638dedd5c1d46264627348ce7c961
zaw htet aung <zawhtetaung.zhared@gmail.com>
2022-07-22 16:18:18 +0630

    deploy prepare

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes)
 app.use('/user', userRoutes)
+
+app.get('/', (req, res) => {
+    res.send(' APP IS RUNNING. ');
+})
+
 // const CONNECTION_URL = 'mongodb+srv://zawhtetaung:january199816@cluster0.q6dwsvf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
